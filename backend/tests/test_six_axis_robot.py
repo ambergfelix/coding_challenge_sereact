1f2a677db3c3c3f753c3f80e31ef0a5263101bd9
Felix Amberg <f.amberg@campus.tu-berlin.de>
2025-06-27 00:36:43 +0200

    test: add unit test for six axis robot arm representation

---
+++ backend/tests/test_six_axis_robot.py
@@ -0,0 +1,42 @@
+import pytest
+from robot.joint import Joint
+from robot.six_axis_robot import Six_axis_robot
+
+
+def test_joint_within_limit():
+    joint = Joint("Test", -45, 45)
+    joint.set_angle(25)
+    assert joint.get_angle() == 25
+
+def test_joint_above_limit():
+    limit = 45
+    joint = Joint("Test", -limit, limit)
+    joint.set_angle(90)
+    assert joint.get_angle() == limit
+
+def test_joint_below_limit():
+    limit = 45
+    joint = Joint("Test", -limit, limit)
+    joint.set_angle(-90)
+    assert joint.get_angle() == -limit
+
+def test_joint_repr():
+    joint = Joint("Test", -180, 180)
+    joint.set_angle(90)
+    assert repr(joint) == "<Joint Test: 90.0 deg>"
+
+# Test Six_axis_robot
+
+def set_robot_joint_angles():
+    robot = Six_axis_robot()
+    angles = [20, -30, 20, -30, 20]
+    robot.set_angles(angles)
+    actual_angles = robot.get_joint_states()
+    assert actual_angles == angles
+
+def test_six_axis_robot_invalid_input_lenght():
+    robot = Six_axis_robot()
+    angles = [90, -90, 90, -90]
+    with pytest.raises(ValueError):
+        robot.set_joint_angles(angles)
+
