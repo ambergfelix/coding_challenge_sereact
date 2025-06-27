18c70206552de79b6018273a5d8470cbc311a057
Felix Amberg <f.amberg@campus.tu-berlin.de>
2025-06-27 11:17:07 +0200

    test: add unit test for forward kinematics of six_axis_robot

--- backend/tests/test_six_axis_robot.py
+++ backend/tests/test_six_axis_robot.py
@@ -27,18 +27,39 @@ def test_joint_repr():
 
 # Test Six_axis_robot
 
-def set_robot_joint_angles():
+def test_set_robot_joint_angles():
     robot = Six_axis_robot()
-    angles = [20, -30, 20, -30, 20]
-    robot.set_angles(angles)
-    actual_angles = robot.get_joint_states()
+    angles = [20, -30, 20, -30, 20, 30]
+    robot.set_joint_angles(angles)
+
+    # Extract float angles from joint objects
+    actual_angles = [j.get_angle() for j in robot.joints]
+
     assert actual_angles == angles
+
+
 
 def test_six_axis_robot_invalid_input_lenght():
     robot = Six_axis_robot()
     angles = [90, -90, 90, -90]
     with pytest.raises(ValueError):
         robot.set_joint_angles(angles)
+
+def test_pose_for_zero_angles():
+    robot = Six_axis_robot()
+    zero_angles = [0,0,0,0,0,0]
+    robot.set_joint_angles(zero_angles)
+    # Get x,y,z coordinates of endefector
+    pose = robot.pose.t_3_1
+    x,y,z = pose.flatten()
+    
+    expected_x = -0.81725
+    expected_y = -0.19145
+    expected_z = -0.005491
+
+    assert x == pytest.approx(expected_x, abs=1e-6)
+    assert y == pytest.approx(expected_y, abs=1e-6)
+    assert z == pytest.approx(expected_z, abs=1e-6)
 
 def test_six_axis_repr_output():
     robot = Six_axis_robot()
@@ -55,5 +76,5 @@ def test_six_axis_repr_output():
     assert len(lines) == 6
     for name, angle, line in zip(expected_names, test_angles, lines):
         assert name in line
-        assert f"{angle} deg" in line
+        assert f"{angle:.1f} deg" in line
 
