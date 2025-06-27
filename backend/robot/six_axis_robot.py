0152ef383ce58428801d8b98e06b9157481a1df6
Felix Amberg <f.amberg@campus.tu-berlin.de>
2025-06-27 11:15:45 +0200

    feat: add DH-Parameters for forwoard kinematics to robot representation

--- backend/robot/six_axis_robot.py
+++ backend/robot/six_axis_robot.py
@@ -1,7 +1,7 @@
 from robot.joint import Joint
+from visual_kinematics.RobotSerial import RobotSerial
 from typing import List
-
-
+import numpy as np
 
 class Six_axis_robot:
     def __init__(self):
@@ -13,34 +13,36 @@ class Six_axis_robot:
             Joint("Wrist2", -180, 180),
             Joint("Wrist3", -180, 180)
         ]
+
+        # Define DH parameters for kinematics
+        # DH parameters taken from Universal Robots UR5
+        self.dh_parameters = np.array([
+           # d [m]      a [m]       alpha [rad] theta [rad] 
+            [0.089159,  0,          np.pi/2,    0],
+            [0,         -0.425,     0,          0],
+            [0,         -0.39225,   0,          0],
+            [0.10915,   0,          np.pi/2,    0],
+            [0.09465,   0,          -np.pi/2,   0],
+            [0.0823,    0,          0,          0]
+        ])
+
+        self.model = RobotSerial(self.dh_parameters)
+        self.pose = self.model.forward([0]*6)
     
-    def set_joint_angles(self, angles: List[float]):
-        if len(angles) != len(self.joints):
+    def set_joint_angles(self, angles_deg: List[float]):
+        if len(angles_deg) != len(self.joints):
             raise ValueError("Angle list does not match joint count.")
-        for joint, angle in zip(self.joints, angles):
+        for joint, angle in zip(self.joints, angles_deg):
             joint.set_angle(angle)
+
+        angles_rad = [np.radians(a) for a in angles_deg]
+        self.pose = self.model.forward(angles_rad)
 
     def get_joint_states(self) -> List[str]:
         return [repr(joint) for joint in self.joints]
 
     def __repr__(self):
         return "\n".join(self.get_joint_states())
-    
-    def test_six_axis_repr_output():
-        robot = Six_axis_robot()
-        # Set a known configuration
-        test_angles = [10, -20, 30, -40, 50, -60]
-        robot.set_joint_angles(test_angles)
 
-        # Get the string representation
-        repr_output = repr(robot)
-        lines = repr_output.strip().split("\n")
-
-        # Check that each line contains the corresponding joint name and angle
-        expected_names = ["Base", "Shoulder", "Elbow", "Wrist1", "Wrist2", "Wrist3"]
-        assert len(lines) == 6
-        for name, angle, line in zip(expected_names, test_angles, lines):
-            assert name in line
-            assert f"{angle} deg" in line
 
 
