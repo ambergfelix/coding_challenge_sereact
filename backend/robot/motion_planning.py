30ab0918f8a1d1fe307970682998d262f739aaff
Felix Amberg <f.amberg@campus.tu-berlin.de>
2025-06-30 17:45:30 +0200

    feat: extend cubic spline interpolation method to incorporate one or more custom viapoints

--- backend/robot/motion_planning.py
+++ backend/robot/motion_planning.py
@@ -6,8 +6,10 @@ trajectories based on linear interpolation.
 """
 
 import numpy as np
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from backend.robot.six_axis_robot import SixAxisRobot
+from backend.robot.collision_checker import CollisionChecker
+import backend.utils.utils as util
 import time
 from scipy.interpolate import CubicSpline
 import math
@@ -15,10 +17,10 @@ import math
 PI = math.pi
 
 
-def cubic_spline_interpolation(start_angles: List[float], goal_angles: List[float], n_steps: int, joint_limits: List[Tuple[float, float]]) -> List[List[float]]:
+def cubic_spline_interpolation(start_angles: List[float], goal_angles: List[float], n_steps: int, joint_limits: List[Tuple[float, float]], via_points: Optional[List[List[float]]] = None) -> List[List[float]]:
     """
     Generates a joint-space trajectory between two angle configurations using cubic spline interpolation
-    with a via point at the midpoint. The function ensures joint limits are respected and selects the
+    with one or more points in between. The function ensures joint limits are respected and selects the
     shortest feasible rotational path when possible.
 
     If the shortest path would violate joint limits, the longer alternative path is used instead.
@@ -33,6 +35,8 @@ def cubic_spline_interpolation(start_angles: List[float], goal_angles: List[floa
         Number of interpolation steps between start and goal configurations
     joint_limits : List[Tuple[float, float]]
         List of (min, max) angle bounds for each joint in radians
+    via_points : Optional[List[List[float]]] = None
+        Optional list of via points for interpolation, if none are passed, the middle of the path is set as the only viapoint
 
     Returns
     -------
@@ -47,27 +51,28 @@ def cubic_spline_interpolation(start_angles: List[float], goal_angles: List[floa
       and only falls back to the longer path if required to stay within limits.
     """
 
-    var_indep = [0, 0.5, 1]
     steps = []
-    print(joint_limits)
-    for start, goal, limits in zip(start_angles, goal_angles, joint_limits):
+
+    if via_points is None or len(via_points) == 0:
+        via_points = [[] for x in range(len(start_angles))]
+
+    for start, goal, limits, points in zip(start_angles, goal_angles, joint_limits, via_points):
         low, high = limits
+        viapoint_given = False if points==[] else True
 
         # Clamp goal angle to joint's rotational limit if it is outside the limits
         if goal > high:
             goal = high
         elif goal < low:
             goal = low
-
-
-
+        
         diff = goal - start
         
         # Adjust difference to the shortest rotation angle:
         # If diff > pi (180°), rotating forward is longer than going backward,
         # so subtract 2pi (360°) to rotate counterclockwise (negative direction).
-        # If diff < -180°, rotating backward is longer than going forward,
-        # so add 360° to rotate clockwise (positive direction).
+        # If diff < -pi (-180°), rotating backward is longer than going forward,
+        # so add 2pi (360°) to rotate clockwise (positive direction).
         if diff > PI:
             diff_shortest = diff - 2*PI
         elif diff < -PI:
@@ -75,31 +80,80 @@ def cubic_spline_interpolation(start_angles: List[float], goal_angles: List[floa
         else:
             diff_shortest = diff
 
-        viapoint = start + (diff_shortest / 2)
         end = start + diff_shortest
-        var_dep = [start, viapoint, end]
+
+        
+
+        if not viapoint_given:
+            var_indep = [0, 0.5, 1]
+            points = [start + (diff_shortest / 2)]
+        else:
+            # Calculate independent variables to achieve equal angle speed
+            all_points = [start] + points + [goal]
+            distances = [abs(all_points[i+1] - all_points[i]) for i in range(len(all_points)-1)]
+            sum_distance = sum(distances)
+            distances_norm = np.array(distances) / sum_distance
+            var_indep = np.insert(np.cumsum(distances_norm), 0, 0)
+
+        var_dep = [start] + points + [end]
 
         spline = CubicSpline(var_indep, var_dep)
         t_values = np.linspace(0, 1, n_steps)
         angle_values = spline(t_values)
         
-        if all(low <= v <= high for v in angle_values):
+        if all(low <= util.normalize_angle(v) <= high for v in angle_values):
             steps.append(angle_values)
         else: 
             # Rotation in other direction using longer path
-            viapoint = start + (diff / 2)
+            if not viapoint_given:
+                viapoint = start + (diff / 2)
+                end = start + diff
+                var_dep = [start, viapoint, end]
             end = start + diff
-            var_dep = [start, viapoint, end]
+            var_dep = [start] + points + [end]
 
             spline = CubicSpline(var_indep, var_dep)
             angle_values = spline(t_values)
             steps.append(angle_values)
 
-
-    
     # Transpose to get list of joint angle configurations at each step
     steps_t = [list(step) for step in zip(*steps)]
     return steps_t
+
+def linear_interpolation_collision_avoidance(start_angles: List[float], goal_angles: List[float], n_steps: int) -> List[List[float]]:
+    via_configs = []
+    perturb_range = 0.2
+    collision_configs = []
+    
+    path = linear_interpolation(start_angles, goal_angles, n_steps)
+    # Simualte path execution
+    # robot_sim = SixAxisRobot()
+    # robot_sim.set_joint_angles(start_angles)
+    path_urdf = ""
+    collision_check = CollisionChecker(path_urdf)
+    collision_check.set_angles(start_angles)
+
+    for step in path:
+        collision_check.set_angles(step)
+        # robot_sim.set_joint_angles(step)
+        if collision_check.check_self_collision():
+            collision_configs.append(step)
+        if collision_check.check_environment_collision():
+            collision_configs.append(step)
+    
+    for config in collision_configs:
+        via_configs.append(config + np.random.uniform(-perturb_range, perturb_range, size=step.shape))
+
+    # Verify if new trajectory is collision free
+    path_new = linear_interpolation()
+        
+
+
+
+            
+
+
+
     
 
 def linear_interpolation(start_angles: List[float], goal_angles: List[float], n_steps: int) -> List[List[float]]:
@@ -135,10 +189,10 @@ def linear_interpolation(start_angles: List[float], goal_angles: List[float], n_
         diff = goal - start
         
         # Adjust difference to the shortest rotation angle:
-        # If diff > 180°, rotating forward is longer than going backward,
-        # so subtract 360° to rotate counterclockwise (negative direction).
-        # If diff < -180°, rotating backward is longer than going forward,
-        # so add 360° to rotate clockwise (positive direction).
+        # If diff > pi (180°), rotating forward is longer than going backward,
+        # so subtract 2pi (360°) to rotate counterclockwise (negative direction).
+        # If diff < -pi (-180°), rotating backward is longer than going forward,
+        # so add 2pi (360°) to rotate clockwise (positive direction).
         if diff > PI:
             diff -= 2*PI
         elif diff < -PI:
